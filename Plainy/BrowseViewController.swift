1d9b608f232862fb677631b73f038a147e9bc121
Martin Hartl <martin@hartl.co>
2017-12-09 08:32:21 +0100

    Refactor menuoutlineview in another file

--- Plainy/BrowseViewController.swift
+++ Plainy/BrowseViewController.swift
@@ -289,28 +289,3 @@ extension BrowseViewController: NSOutlineViewDataSource, MenuOutlineViewDelegate
         }
     }
 }
-
-protocol MenuOutlineViewDelegate: NSOutlineViewDelegate {
-    func outlineView(outlineView: NSOutlineView, menuForItem item: Any) -> NSMenu?
-
-    func outlineView(menuForNoItemIn outlineView: NSOutlineView) -> NSMenu?
-}
-
-class MenuOutlineView: NSOutlineView {
-    override func menu(for event: NSEvent) -> NSMenu? {
-        guard let delegate = delegate as? MenuOutlineViewDelegate else { return nil }
-
-        let point = convert(event.locationInWindow, from: nil)
-        let row = self.row(at: point)
-        let item = self.item(atRow: row)
-
-        selectRowIndexes([row], byExtendingSelection: false)
-
-        if (item == nil) {
-            deselectAll(nil)
-            return delegate.outlineView(menuForNoItemIn: self)
-        }
-
-        return delegate.outlineView(outlineView: self, menuForItem: item!)
-    }
-}
